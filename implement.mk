0f0ebfad0b9a4df70184ad6c7e5f2eec2e5101ba
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-21 18:23:45 +0300

    Update implement.mk

--- implement.mk
+++ implement.mk
@@ -15,3 +15,18 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Install NeoGSI post-fs-data hook
+PRODUCT_COPY_FILES += \
+    vendor/ngsi/ngsi.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/ngsi.rc \
+    vendor/ngsi/ngsi-post-fs-data.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/ngsi-post-fs-data.sh
+
+# Do some workarounds for Pie / Quince Tart devices that will fix potential bootloops
+PRODUCT_EXTRA_VNDK_VERSIONS += 28 29
+
+# SELinux schenanigans
+SELINUX_IGNORE_NEVERALLOWS := true
+
+# Install the NeoGSI version of the resetprop
+PRODUCT_PACKAGES += \
+    resetprop_ngsi
