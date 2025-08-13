e72f7998f7eb2ecb968d35c0dab552e28d86e92a
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-08-13 13:25:46 +0300

    Finish checkup.sh

--- payloads/0000deviceinfo.payload/checkup.sh
+++ payloads/0000deviceinfo.payload/checkup.sh
@@ -1,2 +1,10 @@
 #!/system/bin/sh
-exit 0
+
+# Fix_GSI_Identity_Crisis is one of my modules, 
+# and it conflicts with this payload.
+# (This payload is a basic version of that module)
+if [ -d "/data/adb/modules/fix_gsi_identity_crisis" ]; then
+  exit 1
+else
+  exit 0
+fi
